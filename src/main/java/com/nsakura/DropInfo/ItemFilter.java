50346dc936ba930ac958e7cbd4dd24ceddcb7277
Li Keqing <me@kaze.ai>
2020-06-25 13:02:45 +0800

    upgrade to 1.16.1. 1.0.5

--- src/main/java/com/nsakura/DropInfo/ItemFilter.java
+++ src/main/java/com/nsakura/DropInfo/ItemFilter.java
@@ -30,46 +30,53 @@ public enum ItemFilter {
             case MUSIC_DISC_FAR:
             case MUSIC_DISC_MALL:
             case MUSIC_DISC_MELLOHI:
+            case MUSIC_DISC_PIGSTEP:
             case MUSIC_DISC_STAL:
             case MUSIC_DISC_STRAD:
             case MUSIC_DISC_WARD:
             case MUSIC_DISC_11:
             case MUSIC_DISC_WAIT:
                 return MUSIC_DISC;
-            case TURTLE_HELMET:
-            case IRON_SHOVEL:
-            case IRON_PICKAXE:
-            case IRON_AXE:
             case FLINT_AND_STEEL:
+            case FISHING_ROD:
+            case SHEARS:
+            case TURTLE_HELMET:
             case WOODEN_SHOVEL:
             case WOODEN_PICKAXE:
             case WOODEN_AXE:
+            case WOODEN_HOE:
             case STONE_SHOVEL:
             case STONE_PICKAXE:
             case STONE_AXE:
-            case DIAMOND_SHOVEL:
-            case DIAMOND_PICKAXE:
-            case DIAMOND_AXE:
+            case STONE_HOE:
+            case IRON_SHOVEL:
+            case IRON_PICKAXE:
+            case IRON_AXE:
+            case IRON_HOE:
             case GOLDEN_SHOVEL:
             case GOLDEN_PICKAXE:
             case GOLDEN_AXE:
-            case WOODEN_HOE:
-            case STONE_HOE:
-            case IRON_HOE:
-            case DIAMOND_HOE:
             case GOLDEN_HOE:
-            case FISHING_ROD:
-            case SHEARS:
+            case DIAMOND_SHOVEL:
+            case DIAMOND_PICKAXE:
+            case DIAMOND_AXE:
+            case DIAMOND_HOE:
+            case NETHERITE_SHOVEL:
+            case NETHERITE_PICKAXE:
+            case NETHERITE_AXE:
+            case NETHERITE_HOE:
                 return TOOLS;
             case BOW:
-            case IRON_SWORD:
+            case CROSSBOW:
+            case TRIDENT:
             case WOODEN_SWORD:
             case STONE_SWORD:
-            case DIAMOND_SWORD:
+            case IRON_SWORD:
             case GOLDEN_SWORD:
-            case TRIDENT:
-            case CROSSBOW:
+            case DIAMOND_SWORD:
+            case NETHERITE_SWORD:
                 return WEAPON;
+            case SHIELD:
             case ELYTRA:
             case LEATHER_HELMET:
             case LEATHER_CHESTPLATE:
@@ -83,15 +90,18 @@ public enum ItemFilter {
             case IRON_CHESTPLATE:
             case IRON_LEGGINGS:
             case IRON_BOOTS:
-            case DIAMOND_HELMET:
-            case DIAMOND_CHESTPLATE:
-            case DIAMOND_LEGGINGS:
-            case DIAMOND_BOOTS:
             case GOLDEN_HELMET:
             case GOLDEN_CHESTPLATE:
             case GOLDEN_LEGGINGS:
             case GOLDEN_BOOTS:
-            case SHIELD:
+            case DIAMOND_HELMET:
+            case DIAMOND_CHESTPLATE:
+            case DIAMOND_LEGGINGS:
+            case DIAMOND_BOOTS:
+            case NETHERITE_HELMET:
+            case NETHERITE_CHESTPLATE:
+            case NETHERITE_LEGGINGS:
+            case NETHERITE_BOOTS:
                 return EQUIPMENT;
             default:
                 return REGULAR;
